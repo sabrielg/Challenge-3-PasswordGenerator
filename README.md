8ca6a755fa2ee77d1ddad9f7331c800f2b8e43bf
Sabriel <sabrielkristine@gmail.com>
2021-11-08 20:22:38 -0700

    clean up and README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Challenge-3-PasswordGenerator
+
+## Description
+- It is challenging to come up with new passwords over and over again. Creating a random password generator is a useful way to ensure passwords are secure and frees the user from having to re-use passwords or come up with new ones ove and over.
+- I was motivated to create a random password generator for users to create secure passwords.
+- Using almost 100% JavaScript helped me learn how to use functions, for loops, arrays, and other elements. 
+## Usage
+This password generator prompts the user to pick how many characters they would like their password to be (between 8-128), asks if they would like special characters, uppcase letters, lowercase letters, and numbers. It collects all of that information in a larger array that then generates a password visible for the user.
+## Credits
+I worked with my classmate Scott Nichols and  my tutor Armando Osario to build and edit this code.
--- script.js
+++ script.js
@@ -2,60 +2,8 @@
 var specialCharacters = [ '@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',
 ']','[','~','-','_','.',];
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var lowerCasedCharacters = ['a','b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
-];
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-];
+var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
 var finalPassword = []
 var generateBtn = document.querySelector("#generate");
 
@@ -67,7 +15,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 function generatePassword() {
